93f3d6fce802faab15d4dcb1d7ea9a07cd6e92a4
Ankush Rana <90180315+ankushrana981@users.noreply.github.com>
2024-05-17 12:41:12 +0530

    Add files via upload

--- src/app/components/shared/constants/urls.ts
+++ src/app/components/shared/constants/urls.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000/api/';
+const BASE_URL = 'http://localhost:8080/api/';
 
 export const FOODS_URL = BASE_URL + 'foods';
 export const FOODS_TAGS_URL = BASE_URL + 'tags';
@@ -8,3 +8,6 @@ export const FOODS_BY_ID_URL = FOODS_URL + '/';
 export const USER_LOGIN_URL = BASE_URL + 'user/login';
 export const USER_REGISTER_URL = BASE_URL + 'user/register';
 
+export const ORDERS_URL = BASE_URL + 'orders';
+export const ORDER_CREATE_URL = ORDERS_URL + '/create';
+export const ORDER_NEW_FOR_CURRENT_USER_URL = ORDERS_URL + '/newOrderForCurrentUser';
