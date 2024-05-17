5f469898256057e5bccb0230f6e0d26e19d0c13e
Ankush Rana <90180315+ankushrana981@users.noreply.github.com>
2024-05-17 12:33:22 +0530

    Add files via upload

--- src/app/components/shared/map/map.component.ts
+++ src/app/components/shared/map/map.component.ts
@@ -1,20 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { GoogleMapsModule, MapAdvancedMarker, MapInfoWindow } from '@angular/google-maps';
+import { } from 'googlemaps'
+import { LocationService } from '../../../services/location.service';
+import { Order } from '../models/order';
+
 
 @Component({
   selector: 'map',
   standalone: true,
-  imports: [],
+  imports: [GoogleMapsModule, MapAdvancedMarker, MapInfoWindow],
+  providers: [LocationService],
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
-export class MapComponent implements OnInit{
-  
-constructor(){
-
-}
-ngOnInit(): void {
+export class MapComponent implements OnInit {
+  @Input()
+  order!:Order;
+  @Input()
+  readonly = false;
+  display: any;
+  private readonly MARKER_ZOOM_LEVEL = 16;
  
-}
-  
-    
+  position:google.maps.LatLngLiteral = { 
+    lat: 0,
+    lng: 0
   }
+  center: google.maps.LatLngLiteral = {
+    lat: 30.2736308,
+    lng: 76.7512555
+  };
+  options: google.maps.MapOptions = {
+    mapId: "197f5132238347bf",
+    center: { lat: -31, lng: 147 },
+    zoom: 15,
+  };
+  @ViewChild(MapInfoWindow) infoWindow!: MapInfoWindow;
+  constructor(private locationService: LocationService) {
+
+  }
+  ngOnInit(): void {
+  }
+  onMarkerClick(marker: MapAdvancedMarker) {
+    this.infoWindow.openAdvancedMarkerElement(marker.advancedMarker, marker.advancedMarker.title);
+  }
+  moveMap(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) this.center = (event.latLng.toJSON());
+  }
+  move(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) this.display = event.latLng.toJSON();
+  }
+  findMyLocation() {
+    this.locationService.getCurrentLocation().subscribe({
+      next:(latLng)=>{
+        console.log(latLng, "current pos")
+        this.center = { lat: latLng.lat, lng: latLng.lng };
+        this.position={lat: latLng.lat, lng: latLng.lng};
+        // set address val 
+        latLng.lat = parseFloat(latLng.lat.toFixed(8));
+        latLng.lng = parseFloat(latLng.lng.toFixed(8));
+        console.log(this.options,"options")
+        this.order.addressLatLng = latLng;
+      }
+    }
+    )
+  }
+}
---
+++ src/app/components/shared/paypal-button/paypal-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paypal-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './paypal-button.component.html',
+  styleUrl: './paypal-button.component.css'
+})
+export class PaypalButtonComponent {
+
+}
